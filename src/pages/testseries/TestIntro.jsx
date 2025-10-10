cf382523377933e6a2231a883c80865f68b99de0
sagar50802 <sagartripathi1311@gmail.com>
2025-10-10 15:30:34 +0530

    Update TestIntro.jsx

--- src/pages/testseries/TestIntro.jsx
+++ src/pages/testseries/TestIntro.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getJSON } from "@/utils/api";
+import { getJSON } from "../../utils/api";
 import { Loader2 } from "lucide-react";
 
 export default function TestIntro({ code }) {
