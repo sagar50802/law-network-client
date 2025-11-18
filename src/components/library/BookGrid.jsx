07c33420f5a35bfd788b15960066e27d543bf79f
sagar50802 <sagartripathi1311@gmail.com>
2025-11-18 17:37:01 +0530

    Update BookGrid.jsx

--- src/components/library/BookGrid.jsx
+++ src/components/library/BookGrid.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BookCard from "./BookCard.jsx";
 
-export default function BookGrid({ books, onSelectBook }) {
+export default function BookGrid({ books = [], onSelectBook }) {
   if (!books || books.length === 0) {
     return (
       <div className="mt-8 text-center text-sm text-slate-400">
@@ -11,12 +11,14 @@ export default function BookGrid({ books, onSelectBook }) {
   }
 
   return (
-    <div className="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+    <div className="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4">
       {books.map((book) => (
         <BookCard
           key={book._id}
           book={book}
-          onClick={() => onSelectBook(book)}
+          onClick={() => {
+            if (onSelectBook) onSelectBook(book);
+          }}
         />
       ))}
     </div>
