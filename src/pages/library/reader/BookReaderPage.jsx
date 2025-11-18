0e2de0c419761905768685fb8ead8b92a8710c75
sagar50802 <sagartripathi1311@gmail.com>
2025-11-18 14:54:06 +0530

    Update BookReaderPage.jsx

--- src/pages/library/reader/BookReaderPage.jsx
+++ src/pages/library/reader/BookReaderPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import * as pdfjsLib from "pdfjs-dist";
-import "../../pdf-worker";
+import "../../../pdf-worker";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
